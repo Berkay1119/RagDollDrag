dfe988ca2c23674a96dc6173504961bed12254ee
Berkay Karakaya <berkay11karakaya@gmail.com>
2022-11-29 16:14:05 +0300

    Personal Computer Version

---
+++ RagDollPractice/Assets/Scripts/DragRagDoll.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DragRagDoll : MonoBehaviour
+{
+    float speed = 10f;
+    GameObject selected;
+    float vector_x;
+    float vector_y;
+    int layerMask = 1 << 6;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount!=0 && selected==null)
+        {
+            Selected();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Input.touchCount == 0)
+        {
+            selected = null;
+        }
+        if (selected != null)
+        {
+            Move();
+        }
+    }
+
+    void Selected()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            selected = hit.transform.gameObject;
+        }
+    }
+    
+    void Move()
+    {
+        Vector3 position = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+        vector_x = (worldPosition - selected.transform.position).x;
+        vector_y = (worldPosition - selected.transform.position).y;
+        selected.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(vector_x, vector_y, 0) * speed;
+    }
+}
